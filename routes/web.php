f0b6d1eae1943a068ea0bcfb233696c7cefd13c5
Dimasaqil <dimasaqilsalsabil@gmail.com>
2024-03-29 13:28:02 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\GajiController;
 use App\Http\Controllers\JabatanController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/presensi', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index']);
 Route::get('/gaji', [GajiController::class, 'index']);
 Route::get('/jabatan', [JabatanController::class, 'index']);
 Route::get('/presensi', [PresensiController::class, 'index']);
